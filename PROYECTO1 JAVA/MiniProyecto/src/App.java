8536f8e35bf92787701868e284fc0fc0731fbc35
MiguelSanclemente <miguel.sanclemente@correounivalle.edu.co>
2025-03-28 18:40:56 -0500

    Plantilla de clases, con setter y getters, cada uno con su respectivo constructor

---
+++ PROYECTO1 JAVA/MiniProyecto/src/App.java
@@ -0,0 +1,37 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // Pokemones de tipo electrico
+        Pokemon picachu = new Pokemon("Pikachu", Tipo.Electrico, null);
+        Pokemon jolteon = new Pokemon("Jolteon", Tipo.Electrico, null);
+        Pokemon raichu = new Pokemon("Raichu", Tipo.Electrico, null);
+
+        //Pokemones de tipo agua
+
+        Pokemon squirtel = new Pokemon("Squirtel", Tipo.Agua, null);
+        Pokemon gyarados = new Pokemon("Gyarados", Tipo.Agua, null);
+        Pokemon vaporeon = new Pokemon("Vaporeon", Tipo.Agua, null);
+
+        // Pokemones de tipo fuego
+
+        Pokemon charizard = new Pokemon("Charizard", Tipo.Agua, null);
+        Pokemon magmar = new Pokemon("Magmar", Tipo.Agua, null);
+        Pokemon arcanine = new Pokemon("Arcanine", Tipo.Agua, null);
+
+        // Pokemones de tipo planta
+
+        Pokemon vaporeon = new Pokemon("Vaporeon", Tipo.Agua, null);
+        Pokemon vaporeon = new Pokemon("Vaporeon", Tipo.Agua, null);
+        Pokemon vaporeon = new Pokemon("Vaporeon", Tipo.Agua, null);
+
+        //Pokemones de tipo hada
+
+        Pokemon vaporeon = new Pokemon("Vaporeon", Tipo.Agua, null);
+        Pokemon vaporeon = new Pokemon("Vaporeon", Tipo.Agua, null);
+        Pokemon vaporeon = new Pokemon("Vaporeon", Tipo.Agua, null);
+
+        
+
+
+    }
+}
